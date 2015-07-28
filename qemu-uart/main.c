5db4066233143c0ecdecf572544d82a6f15aa716
finallyjustice <dongli.zhang0129@gmail.com>
2015-07-27 20:54:58 -0700

    add uart on qemu

--- qemu-uart/main.c
+++ qemu-uart/main.c
@@ -2,10 +2,18 @@
 
 unsigned int  *uart_base = (unsigned int *)UART_BASE;
 
+void uart_send(unsigned char c)
+{
+	*uart_base = c;
+	if(c == '\n')
+		*uart_base = '\r';
+}
+
 void bootmain(void)
 {
-	uart_base[0] = 0x45;
-	
+	uart_send('Y');
+	uart_send('\n');
+
 	while(1)
 	return;
 }
--- qemu-uart/run.sh
+++ qemu-uart/run.sh
@@ -1,3 +1,3 @@
 #Foundation_Platform --cores=4 --image kernel.axf
-
 /soft/qemu/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 -smp 4 -m 4096 -kernel kernel.axf -serial stdio 
+#/soft/qemu/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 -m 4096 -kernel kernel.axf -serial stdio -gdb tcp::1234 -S
